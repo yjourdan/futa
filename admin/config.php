8a9942ecc9f5d2c6edcc2caecbcfbef46202d072
Yann Jourdan <yjourdan@Home.local>
2016-11-17 23:53:25 +1300

    Finished installation

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://futa.localhost/admin/');
+define('HTTP_CATALOG', 'http://futa.localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://futa.localhost/admin/');
+define('HTTPS_CATALOG', 'http://futa.localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/futa/admin/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/futa/image/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/futa/admin/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/futa/admin/view/template/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/config/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/XAMPP/xamppfiles/htdocs/futa/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'futa');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://futa.localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://futa.localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/futa/catalog/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/futa/image/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/futa/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/futa/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/config/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/htdocs/futa/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'futa');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
